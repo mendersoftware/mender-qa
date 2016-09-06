b1a07197933eec05baeede8a82f3fb355919b8b2
Kristian Amlie <kristian.amlie@mender.io>
2016-09-06 09:54:28 +0200

    Move port 222 forwarding to separate script.
    We should use port 222 forwarding on all hosts, not just proxy hosts,
    for the reasons described in the comments of this change. Therefore we
    create the new script initialize-user-data.sh, which we should run on
    all provisioned hosts. This means one less step to do inside
    nested-vm.sh, and we can centralize the steps in the user-data
    section, instead of specifying it over and over for every host.
    
    Signed-off-by: Kristian Amlie <kristian.amlie@mender.io>

---
+++ scripts/initialize-user-data.sh
@@ -0,0 +1,27 @@
+#!/bin/false
+
+# This file should be sourced, not run.
+
+# This script will do build slave setup, including creating credentials for the
+# jenkins user, based on root's credentials (will copy its keys). The script is
+# expected to be sourced early in the user-data phase after provisioning.
+
+# It will also create a port forwarding rule from port 222 to localhost:22. This
+# is equivalent to logging in on port 22, but the reason this is necessary is to
+# stop Jenkins from logging in too early. If it tries to login too early, it
+# will find the port open, but the key for the jenkins user might not be
+# accepted yet, and it will give up. However, if we keep port 222 closed until
+# we know it's ready, it will keep trying and eventually succeed.
+
+useradd -m jenkins
+mkdir -p /home/jenkins/.ssh
+cp /root/.ssh/authorized_keys /home/jenkins/.ssh
+
+echo "jenkins ALL=(ALL:ALL) NOPASSWD:ALL" >> /etc/sudoers
+# Disable TTY requirement.
+sed -i -e 's/^\( *Defaults *requiretty *\)$/# \1/' /etc/sudoers
+
+chown -R jenkins:jenkins /home/jenkins
+
+iptables -t nat -I PREROUTING 1 -p tcp --dport 222 -j DNAT --to-dest :22
+iptables -t nat -I OUTPUT 1 -p tcp --dst 127.0.0.1 --dport 222 -j DNAT --to-dest :22
