f62490b618e2b8319a92a7bad5748a808696ddf5
Kristian Amlie <kristian.amlie@mender.io>
2016-11-11 11:13:42 +0100

    Rewrite the Yocto build scripts according to current Jenkins status.
    This will be called from the Jenkins recipes.
    
    Signed-off-by: Kristian Amlie <kristian.amlie@mender.io>

--- scripts/jenkins-yoctobuild-build.sh
+++ scripts/jenkins-yoctobuild-build.sh
@@ -1,160 +1,134 @@
 #!/bin/bash
+
 set -e -x
 
+echo $WORKSPACE
+#----
+echo "Debug Jenkins setup"
+ls /home/jenkins/.ssh
+#----
 
-export WORKSPACE="/home/jenkins/workspace/yoctobuild/"
-export PATH=${WORKSPACE}/scripts:${WORKSPACE}/bitbake/bin:/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games
-BUILD_DIR=${WORKSPACE}/build/tmp/deploy/images
+export PATH=$WORKSPACE/scripts:$WORKSPACE/bitbake/bin:/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games
+if [ "$CLEAN_BUILD_CACHE" = "true" ]
+then
+    sudo rm -rf /mnt/sstate-cache/*
+fi
 
-#replace filenames with variable
-ORIGINAL_BBB_SDIMG=$BUILD_DIR/beaglebone/core-image-base-beaglebone.sdimg
-ORIGINAL_BBB_EXT3=$BUILD_DIR/beaglebone/core-image-base-beaglebone.ext3
-ORIGINAL_VEXPRESS_SDIMG=$BUILD_DIR/vexpress-qemu/core-image-full-cmdline-vexpress-qemu.sdimg
-ORIGINAL_VEXPRESS_EXT3=$BUILD_DIR/vexpress-qemu/core-image-full-cmdline-vexpress-qemu.ext3
-
-#unmodified copies of our build
-ORIGINAL_BBB_SDIMG_COPY=$(mktemp)
-ORIGINAL_BBB_EXT3_COPY=$(mktemp)
-ORIGINAL_VEXPRESS_SDIMG_COPY=$(mktemp)
-ORIGINAL_VEXPRESS_EXT3_COPY=$(mktemp)
-
-#unmodified vexpress image for docker
-MODIFIED_VEXPRESS_SDIMG="$BUILD_DIR"/vexpress-qemu/core-image-full-cmdline-vexpress-qemu.sdimg.modified
-
-function cleanup() {
-    rm "$ORIGINAL_BBB_EXT3_COPY" "$ORIGINAL_BBB_SDIMG_COPY" "$ORIGINAL_VEXPRESS_EXT3_COPY" "$ORIGINAL_VEXPRESS_SDIMG_COPY"
-}
-
-function create_img_copies() {
-    cp "$ORIGINAL_VEXPRESS_SDIMG" "$ORIGINAL_VEXPRESS_SDIMG_COPY"
-    cp "$ORIGINAL_VEXPRESS_EXT3" "$ORIGINAL_VEXPRESS_EXT3_COPY"
-    cp "$ORIGINAL_BBB_SDIMG" "$ORIGINAL_BBB_SDIMG_COPY"
-    cp "$ORIGINAL_BBB_EXT3" "$ORIGINAL_BBB_EXT3_COPY"
-    trap cleanup EXIT
-}
-
-function remove_file_sdimg() {
-    local sdimg_file=$1
-    local remove_file=$2
-    local partition=$3
-    local mnt_point="/mnt/loop_edit_sdimg/"
-
-    sudo umount ${mnt_point} 2>/dev/null || true
-
-    PART_OFFSET=$(sudo fdisk -l "$sdimg_file" | grep $(basename $sdimg_file)"${partition}" | awk '{sum = $2 * 512; print sum}')
-    sudo mkdir -p ${mnt_point} >/dev/null
-    sudo mount -t ext4 -o loop,offset="${PART_OFFSET}" "${sdimg_file}" ${mnt_point}
-    sudo rm "${mnt_point}""${remove_file}"
-
-    sudo umount ${mnt_point}
-}
-
-function add_file_sdimg() {
-    local sdimg_file=$1
-    local add_file=$2
-    local add_file_path=$3
-    local partition=$4
-    local mnt_point="/mnt/loop_edit_sdimg/"
-
-    sudo umount ${mnt_point} 2>/dev/null || true
-
-    PART_OFFSET=$(sudo fdisk -l "$sdimg_file" | grep $(basename $sdimg_file)"${partition}" | awk '{sum = $2 * 512; print sum}')
-    sudo mkdir -p ${mnt_point} >/dev/null
-    sudo mount -t ext4 -o loop,offset="${PART_OFFSET}" "${sdimg_file}" ${mnt_point}
-    sudo cp "${add_file}" "${mnt_point}""${add_file_path}"
-
-    sudo umount ${mnt_point}
-}
-
-
-function clean_up_build() {
+if [ "$BUILD_QEMU" = "true" ]
+then
     source oe-init-build-env
-    bitbake -c cleanall core-image-full-cmdline
-    MACHINE="beaglebone" bitbake -c cleanall core-image-base
-    rm -rf tmp/
-    rm -rf sstate-cache/
-}
+    cd ../
 
-function do_build() {
-    source oe-init-build-env
-    set +e
-
-    bitbake core-image-full-cmdline
-    export MACHINE="beaglebone"
-    bitbake core-image-base
-    set -e
-}
-
-function setup_build() {
-
-    source oe-init-build-env
-    cd ..
-
-    if [ ! -d meta-mender-qemu ]; then
-        echo "JENKINS SCRIPT: meta-mender-qemu directory is not present"
-        exit 1
+    if [ ! -d mender-qa ]
+    then
+      echo "JENKINS SCRIPT: mender-qa directory is not present"
+      exit 1
     else
-
-    /bin/rm -f build/conf/bblayers.conf
-    echo $?
-    /bin/rm -f build/conf/local.conf
-    echo $?
-    /bin/cp meta-mender-qemu/build-conf/bblayers.conf  build/conf/bblayers.conf
-    echo $?
-    /bin/cp meta-mender-qemu/build-conf/local.conf  build/conf/local.conf
-    echo $?
-    # See comment in local.conf
-    cat >> build/conf/local.conf <<EOF
+      /bin/rm -f $BUILDDIR/conf/*
+      /bin/cp mender-qa/build-conf/*  $BUILDDIR/conf/
+      # See comment in local.conf
+      cat >> $BUILDDIR/conf/local.conf <<EOF
 EXTERNALSRC_pn-mender = "$WORKSPACE/mender"
+EXTERNALSRC_pn-artifacts = "$WORKSPACE/artifacts"
+EXTERNALSRC_pn-artifacts-native = "$WORKSPACE/artifacts"
+SSTATE_DIR = "/mnt/sstate-cache"
 EOF
-fi
-
-
-}
-
-function run_tests() {
-  for i in "$@" ; do
-     if [[ $i == "--qemu" ]] ; then
-        ( cd $WORKSPACE/meta-mender/tests/acceptance/ && py.test || exit 1 )
-     fi
-
-     if [[ $i == "--bbb" ]] ; then
-       ( cd $WORKSPACE/meta-mender/tests/acceptance/ bash prepare_bbb_testing.sh || exit 1 )
-     fi
-  done
-}
-
-# If a build is triggered between 1:00 and 1:59, clean the builds and start from scratch
-if [[ $(date +%H) -eq 1 ]]; then
-    clean_up_build
-fi
-
-for i in "$@" ; do
-    if [[ $i == "--skip-build" ]] ; then
-        break
     fi
 
-    setup_build && do_build
-done
+    source oe-init-build-env
+    bitbake core-image-full-cmdline
 
-if [ "$#" -eq 0 ]; then
-  setup_build && do_build
+    mkdir -p $WORKSPACE/vexpress-qemu
+    cp -L $BUILDDIR/tmp/deploy/images/vexpress-qemu/u-boot.elf $WORKSPACE/vexpress-qemu/u-boot.elf
+    cp -L $BUILDDIR/tmp/deploy/images/vexpress-qemu/core-image-full-cmdline-vexpress-qemu.ext4 $WORKSPACE/vexpress-qemu/core-image-full-cmdline-vexpress-qemu.ext4
+    cp -L $BUILDDIR/tmp/deploy/images/vexpress-qemu/core-image-full-cmdline-vexpress-qemu.sdimg $WORKSPACE/vexpress-qemu/core-image-full-cmdline-vexpress-qemu.sdimg
+    export PATH=$PATH:$BUILDDIR/tmp/sysroots/x86_64-linux/usr/bin
+
+    cd $WORKSPACE/meta-mender/tests/acceptance/
+
+    export UBOOT_ELF=$WORKSPACE/vexpress-qemu/u-boot.elf
+    export VEXPRESS_SDIMG=$WORKSPACE/vexpress-qemu/core-image-full-cmdline-vexpress-qemu.sdimg
+    export QEMU_SYSTEM_ARM="qemu-system-arm"
+    cp $WORKSPACE/vexpress-qemu/core-image-full-cmdline-vexpress-qemu.ext4 image.dat
+
+    artifacts write rootfs-image -t vexpress-qemu -n test-update -u $BUILDDIR/tmp/deploy/images/vexpress-qemu/core-image-full-cmdline-vexpress-qemu.ext4 -o successful_image_update.mender
+    # run tests on qemu
+    if [ "$TEST_QEMU" = "true" ]
+    then
+        py.test
+    fi
+
+    (cd $WORKSPACE/meta-mender && cp -L $BUILDDIR/tmp/deploy/images/vexpress-qemu/u-boot.elf . )
+    (cd $WORKSPACE/meta-mender && cp -L $BUILDDIR/tmp/deploy/images/vexpress-qemu/core-image-full-cmdline-vexpress-qemu.sdimg . )
+    cd $WORKSPACE/
+
+
+    if [ "$UPLOAD_OUTPUT" = "true" ]
+    then
+    # store useful output to directory
+    mkdir -p "vexpress-qemu-deploy"
+        mv $BUILDDIR/tmp/deploy/* "vexpress-qemu-deploy"
+    fi
+
+    rm -rf build
 fi
 
-create_img_copies
+if [ "$BUILD_BBB" = "true" ]
+then
+    source oe-init-build-env
+    cp ../mender-qa/build-conf/*  ./conf/
 
-( cd $WORKSPACE/meta-mender/tests/acceptance/ && rm -f image.dat || true && cp $ORIGINAL_VEXPRESS_EXT3 image.dat)
+    cat >> ./conf/local.conf <<EOF
+EXTERNALSRC_pn-mender = "$WORKSPACE/mender"
+EXTERNALSRC_pn-artifacts = "$WORKSPACE/artifacts"
+EXTERNALSRC_pn-artifacts-native = "$WORKSPACE/artifacts"
+SSTATE_DIR = "/mnt/sstate-cache"
+EOF
 
-run_tests "$@"
+    export MACHINE="beaglebone"
+    bitbake core-image-base
 
-# we need this for the docker image later
-cp "$ORIGINAL_VEXPRESS_SDIMG_COPY" "$MODIFIED_VEXPRESS_SDIMG"
+    mkdir -p $WORKSPACE/beaglebone
 
-# Restore original images with the mender service removed.
-cp "$ORIGINAL_VEXPRESS_SDIMG_COPY" "$ORIGINAL_VEXPRESS_SDIMG"
-cp "$ORIGINAL_VEXPRESS_EXT3_COPY" "$ORIGINAL_VEXPRESS_EXT3"
-cp "$ORIGINAL_BBB_SDIMG_COPY" "$ORIGINAL_BBB_SDIMG"
-cp "$ORIGINAL_BBB_EXT3_COPY" "$ORIGINAL_BBB_EXT3"
+    cp -L $BUILDDIR/tmp/deploy/images/beaglebone/core-image-base-beaglebone.ext4 $WORKSPACE/beaglebone/core-image-base-beaglebone.ext4
+    cp -L $BUILDDIR/tmp/deploy/images/beaglebone/core-image-base-beaglebone.sdimg $WORKSPACE/beaglebone/core-image-base-beaglebone.sdimg
+    cp -L $BUILDDIR/tmp/deploy/images/beaglebone/core-image-base-beaglebone.ext4 $WORKSPACE/beaglebone/core-image-base-beaglebone.ext4.clean
+    cp -L $BUILDDIR/tmp/deploy/images/beaglebone/core-image-base-beaglebone.sdimg $WORKSPACE/beaglebone/core-image-base-beaglebone.sdimg.clean
 
-add_file_sdimg $MODIFIED_VEXPRESS_SDIMG ${WORKSPACE}/meta-mender/recipes-mender/mender/files/mender.service /etc/systemd/system/multi-user.target.wants/ 2
-add_file_sdimg $MODIFIED_VEXPRESS_SDIMG ${WORKSPACE}/meta-mender/recipes-mender/mender/files/mender.service /lib/systemd/system/ 2
+    cd $WORKSPACE/meta-mender/tests/acceptance/
+    export PATH=$PATH:$BUILDDIR/tmp/sysroots/x86_64-linux/usr/bin
+    export BBB_IMAGE_DIR=$WORKSPACE/beaglebone
+
+    if [ "$TEST_BBB" = "true" ]
+    then
+    bash prepare_ext4_testing.sh
+    artifacts write rootfs-image -t beaglebone -n test-update -u core-image-base-beaglebone-modified-testing.ext4 -o successful_image_update.mender
+
+        bash prepare_bbb_testing.sh || {
+            kill -s TERM $(ps aux | grep ssh| grep wmd | awk '{print $2}') || true
+            exit 1;
+    }
+    fi
+
+
+    cp -L $WORKSPACE/beaglebone/core-image-base-beaglebone.ext4.clean $WORKSPACE/beaglebone/core-image-base-beaglebone.ext4
+    cp -L $WORKSPACE/beaglebone/core-image-base-beaglebone.sdimg.clean  $WORKSPACE/beaglebone/core-image-base-beaglebone.sdimg
+
+    # store useful output to directory
+
+    if [ "$UPLOAD_OUTPUT" = "true" ]
+    then
+        cd $WORKSPACE/
+    mkdir -p "beaglebone-deploy"
+        mv $BUILDDIR/tmp/deploy/* "beaglebone-deploy"
+    fi
+fi
+
+if [ "$UPLOAD_OUTPUT" = "true" ]
+then
+    cd $WORKSPACE
+    tar acvf output.tar.xz  --ignore-failed-read "vexpress-qemu-deploy" "beaglebone-deploy"
+    s3cmd put output.tar.xz s3://mender/temp/yoctobuilds/$BUILD_TAG/
+    s3cmd setacl s3://mender/temp/yoctobuilds/$BUILD_TAG/output.tar.xz --acl-public
+    echo "Download build output from: https://s3.amazonaws.com/mender/temp/yoctobuilds/${BUILD_TAG}/output.tar.xz"
+fi
