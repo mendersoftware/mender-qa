c1505f2ddce061f3fcfd07ef468b266a22d2ac8a
Enrico Razzetti <enrico.razzetti@cfengine.com>
2016-11-28 17:18:35 +0100

    changed login user to jenkins

--- scripts/initialize-build-host-centos.sh
+++ scripts/initialize-build-host-centos.sh
@@ -132,7 +132,7 @@ fi
 #alias apt-get=apt_get
 
 address="$(cat $HOME/ip.txt)"
-until ssh $address "uname -a";
+until ssh jenkins@$address "uname -a";
 do
   echo "waiting for solaris to answer to ssh"
   sleep 10
