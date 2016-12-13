ec502fe4644e696816c0166ca592cb45ab6d48aa
Enrico Razzetti <enrico.razzetti@cfengine.com>
2016-12-13 14:28:11 +0100

    added more kvm packages

--- scripts/nested-vm-centos.sh
+++ scripts/nested-vm-centos.sh
@@ -44,7 +44,7 @@ egrep -q '^flags\b.*\bvmx\b' /proc/cpuinfo
 
 # Install KVM and other tools.
 sudo apt -qy update
-sudo apt -qy install libvirt-bin rsync kvm qemu-kvm
+sudo apt -qy install libvirt-bin rsync kvm qemu-kvm qemu-system-x86_64
 
 #sudo yum -y update
 #sudo yum -y install epel-release
