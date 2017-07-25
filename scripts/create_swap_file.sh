94f6993d0b7f17727188e7d617fbbc59c1e52015
Dimitrios Apostolou <jimis@northern.tech>
2017-07-25 12:32:59 +0200

    Create swap file script.

---
+++ scripts/create_swap_file.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+set -e                                                   # exit on error
+
+# Argument $1 is the size in megabytes
+if [ x"$1" = x ]  ||  echo "$1" | grep -q '^[0-9]'
+then
+    exit 2
+fi
+SIZE="$1"
+
+# Don't overwrite if file exists
+[ -f /swapfile ] && exit 1
+
+time dd if=/dev/zero of=/swapfile bs=1M count=$SIZE
+chmod 0600 /swapfile
+mkswap /swapfile
+swapon /swapfile
