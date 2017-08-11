f80e54bec3c6a60343411b13e70049189c622d9a
Greg Di Stefano <greg.distefano+github@gmail.com>
2017-08-11 13:13:28 -0400

    remove build optimization where only microservice in PR is built
    Signed-off-by: Greg Di Stefano <greg.distefano+github@gmail.com>

--- scripts/jenkins-yoctobuild-build.sh
+++ scripts/jenkins-yoctobuild-build.sh
@@ -203,12 +203,6 @@ export GOPATH="$WORKSPACE/go"
     CGO_ENABLED=0 go build
 )
 for build in deployments deviceadm deviceauth inventory useradm; do (
-
-    # If we are testing a specific microservice, only build that one.
-    if [[ -n $REPO_TO_TEST && $build != $REPO_TO_TEST ]]; then
-        continue
-    fi
-
     $WORKSPACE/integration/extra/release_tool.py --set-version-of $build --version pr
     cd go/src/github.com/mendersoftware/$build
     CGO_ENABLED=0 go build
