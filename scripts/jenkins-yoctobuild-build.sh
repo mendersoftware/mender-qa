8b8623f9172c2ddf48161894ee51fb7220615bee
mchalczynski <marcin.chalczynski@rndity.com>
2018-10-18 17:06:59 +0200

    changelog: run both ST and MT backend tests
    Signed-off-by: mchalczynski <marcin.chalczynski@rndity.com>

--- scripts/jenkins-yoctobuild-build.sh
+++ scripts/jenkins-yoctobuild-build.sh
@@ -955,7 +955,11 @@ run_backend_integration_tests() {
             "backend_integration_${INTEGRATION_REV}"
 
         local testing_status=0
-        cd $WORKSPACE/integration/backend-tests && PYTEST_ARGS="-k 'not Multitenant'" ./run || testing_status=$?
+
+        cd $WORKSPACE/integration/backend-tests && \
+           PYTEST_ARGS="-k 'not Multitenant'" ./run && \
+           PYTEST_ARGS="-k Multitenant" ./run -f=../docker-compose.tenant.yml -f=../docker-compose.mt.yml -f=../docker-compose.storage.minio.yml || \
+           testing_status=$?
 
         if [ $testing_status -ne 0 ]; then
             github_pull_request_status \
