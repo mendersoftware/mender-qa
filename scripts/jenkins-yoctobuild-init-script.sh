50d8a3e78034bb233bebdc6bf264c117e3797c26
Kristian Amlie <kristian.amlie@northern.tech>
2018-11-09 14:32:54 +0100

    Switch to apt_get in Yocto init script to work around lock errors.
    Signed-off-by: Kristian Amlie <kristian.amlie@northern.tech>

--- scripts/jenkins-yoctobuild-init-script.sh
+++ scripts/jenkins-yoctobuild-init-script.sh
@@ -13,12 +13,12 @@ chown jenkins:jenkins /usr/bin/docker-compose
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-apt-get -qy update
+apt_get -qy update
 echo "deb http://apt.dockerproject.org/repo debian-jessie main" | tee -a /etc/apt/sources.list.d/docker.list
 curl -sL https://deb.nodesource.com/setup_8.x | -E bash -
-apt-get -qy update
-apt-get -qy --force-yes install git autoconf automake build-essential diffstat gawk chrpath libsdl1.2-dev e2tools nfs-client  s3cmd psmisc screen libssl-dev python-dev libxml2-dev libxslt-dev libffi-dev nodejs libyaml-dev sysbench texinfo default-jre-headless pkg-config zlib1g-dev libaio-dev libbluetooth-dev libbrlapi-dev libbz2-dev libglib2.0-dev libfdt-dev libpixman-1-dev zlib1g-dev jq liblzo2-dev device-tree-compiler qemu-system-x86 bc kpartx
-apt-get -qy --force-yes install docker-ce || apt-get -qy --force-yes install docker-ce
+apt_get -qy update
+apt_get -qy --force-yes install git autoconf automake build-essential diffstat gawk chrpath libsdl1.2-dev e2tools nfs-client  s3cmd psmisc screen libssl-dev python-dev libxml2-dev libxslt-dev libffi-dev nodejs libyaml-dev sysbench texinfo default-jre-headless pkg-config zlib1g-dev libaio-dev libbluetooth-dev libbrlapi-dev libbz2-dev libglib2.0-dev libfdt-dev libpixman-1-dev zlib1g-dev jq liblzo2-dev device-tree-compiler qemu-system-x86 bc kpartx
+apt_get -qy --force-yes install docker-ce || apt_get -qy --force-yes install docker-ce
 cp /sbin/debugfs /usr/bin/ || echo "debugfs not in /sbin/"
 
 wget https://storage.googleapis.com/golang/go1.10.linux-amd64.tar.gz
@@ -33,7 +33,7 @@ npm install -g gulp
 npm install mocha selenium-webdriver@3.0.0-beta-2 saucelabs
 
 # Python 2 pip
-apt-get -qy --force-yes install python-pip
+apt_get -qy --force-yes install python-pip
 pip2 install requests --upgrade
 pip2 install pytest==3.2.5
 pip2 install filelock --upgrade
@@ -45,5 +45,5 @@ pip2 install boto3 --upgrade
 pip2 install pycrypto --upgrade
 
 # Python 3 pip
-apt-get -qy --force-yes install python3-pip
+apt_get -qy --force-yes install python3-pip
 pip3 install pyyaml --upgrade
