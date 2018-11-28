49dde5375686dddce1cc258659f66a32cb86af90
Kristian Amlie <kristian.amlie@northern.tech>
2018-11-28 10:30:16 +0100

    Move step that waits for host readiness to central script for reuse.
    Signed-off-by: Kristian Amlie <kristian.amlie@northern.tech>

--- scripts/jenkins-yoctobuild-build.sh
+++ scripts/jenkins-yoctobuild-build.sh
@@ -2,17 +2,7 @@
 
 set -e -x -E
 
-attempts=180
-while [ $attempts -gt 0 ] && ! systemctl is-system-running; do
-    # Wait for init-script to finish.
-    sleep 10
-    attempts=$(expr $attempts - 1 || true)
-done
-sudo journalctl -u rc-local | cat || true
-
-if [ $attempts -le 0 ]; then
-    exit 1
-fi
+$WORKSPACE/mender-qa/scripts/wait-until-build-host-ready.sh
 
 echo $WORKSPACE
 
