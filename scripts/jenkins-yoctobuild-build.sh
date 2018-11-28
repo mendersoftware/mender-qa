e4bc820e83e408533ea2ca9928b28c9c755f4432
Kristian Amlie <kristian.amlie@northern.tech>
2018-11-28 08:22:06 +0100

    Work around expr returning non zero when result is zero.
    Signed-off-by: Kristian Amlie <kristian.amlie@northern.tech>

--- scripts/jenkins-yoctobuild-build.sh
+++ scripts/jenkins-yoctobuild-build.sh
@@ -6,7 +6,7 @@ attempts=180
 while [ $attempts -gt 0 ] && ! systemctl is-system-running; do
     # Wait for init-script to finish.
     sleep 10
-    attempts=$(expr $attempts - 1)
+    attempts=$(expr $attempts - 1 || true)
 done
 sudo journalctl -u rc-local | cat || true
 
