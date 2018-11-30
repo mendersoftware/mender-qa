47f482fa902a0ee0729343af9e216d76e2fab90c
Kristian Amlie <kristian.amlie@northern.tech>
2018-11-30 08:39:53 +0100

    Revert "Fix unhandled integration repository in upload list."
    Will handle in release_tool instead.
    
    This reverts commit 515cbd51dc6da289afc935a63d2327feefbb7c46.
    
    Signed-off-by: Kristian Amlie <kristian.amlie@northern.tech>

--- scripts/jenkins-yoctobuild-build.sh
+++ scripts/jenkins-yoctobuild-build.sh
@@ -1043,10 +1043,6 @@ if [ "$PUBLISH_ARTIFACTS" = true ]; then
                     # Handled in publish_artifacts().
                     :
                     ;;
-                integration)
-                    # No uploads.
-                    :
-                    ;;
                 *)
                     echo "Don't know how to upload containers for $image!"
                     exit 1
