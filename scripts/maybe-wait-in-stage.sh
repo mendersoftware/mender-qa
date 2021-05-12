49879529d9d71949d4c276acfe8a92233a2207e4
Kristian Amlie <kristian.amlie@northern.tech>
2021-05-12 09:09:31 +0200

    Make it possible to wait in a stage by manual intervention on the host.
    This is handy if you're trying to debug a stage which you are
    retrying. It's impossible to change the environment, but you can log
    into the host and create the WAIT file.
    
    Signed-off-by: Kristian Amlie <kristian.amlie@northern.tech>

--- scripts/maybe-wait-in-stage.sh
+++ scripts/maybe-wait-in-stage.sh
@@ -6,12 +6,11 @@ set -e
 # the variable is "true", then the wait file will be created and the script will
 # wait for as long as it exists.
 
-if [ "$(eval echo \"\$$1\")" != "true" ]; then
-    exit 0
+if [ "$(eval echo \"\$$1\")" = "true" ]; then
+    touch "$2"
 fi
 
-touch "$2"
-echo "$1 == true. Waiting indefinitely until $2 is removed."
 while [ -f "$2" ]; do
+    echo "Waiting indefinitely until $2 is removed."
     sleep 10
 done
