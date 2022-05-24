46698865f7748f0560c89d5b02f13cec582599db
Craig Comstock <craig.comstock@northern.tech>
2022-05-24 14:14:09 -0500

    Add support for id_ed25519 keys for buildslave jenkins user
    ed25519 key is optional, if not present on the host in /root/*id_ed25519
    the key will be skipped.
    
    Ticket: ENT-8623
    Changelog: none

--- scripts/initialize-user-data.sh
+++ scripts/initialize-user-data.sh
@@ -36,12 +36,16 @@ cp -r /root/mender-qa /home/jenkins
 # Key generated for the purpose of connecting to build-artifacts-cache
 # via SFTP and getting the build artifacts (for normal build hosts) or
 # getting the nested VM image (for hosts that run nested-vm.sh).
-privkeyfile=/root/*id_rsa
-privkeybase=`basename $privkeyfile`  ||  true
-[ -f $privkeyfile ] \
-    && cp $privkeyfile       /home/jenkins/.ssh/id_rsa \
-    && chmod 600             /home/jenkins/.ssh/id_rsa \
-    && cp /root/mender-qa/data/$privkeybase.pub /home/jenkins/.ssh/id_rsa.pub
+copy_key() {
+  privkeyfile=/root/*$1
+  privkeybase=$(basename $privkeyfile) || true
+  [ -f $privkeyfile ] \
+    && cp $privkeyfile /home/jenkins/.ssh/$1 \
+    && chmod 600         /home/jenkins/.ssh/$1 \
+    && cp /root/mender-qa/data/$privkeybase.pub /home/jenkins/.ssh/$1.pub
+}
+copy_key id_rsa
+copy_key id_ed25519 || true # as of 2022 June this can be optional
 cp /root/mender-qa/data/known_hosts                    /home/jenkins/.ssh/known_hosts
 
 
