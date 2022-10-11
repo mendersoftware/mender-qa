752ea11f19a97588738b6afcc3cb1926022182c0
Alf-Rune Siqveland <alf.rune@northern.tech>
2022-10-11 10:49:35 +0200

    ci: Update ubuntu kvm image script to Ubuntu:22.04
    Signed-off-by: Alf-Rune Siqveland <alf.rune@northern.tech>

--- scripts/gcloud-kvm-image/update-image.sh
+++ scripts/gcloud-kvm-image/update-image.sh
@@ -8,12 +8,12 @@ set -e
 # -- it might be a good idea to keep at least one backup
 IMAGES_TO_KEEP=2
 
-# Get current and newest available ubuntu-2004-lts image
+# Get current and newest available ubuntu-2204-lts image
 CUR_IMAGES=$(gcloud compute images list --filter="name~'nested-virt.*'" \
                 --sort-by="~creationTimestamp" --format="value(name)" | \
              cut -f1)
 CUR_IMAGE=$(echo $CUR_IMAGES | awk '{print $1}')
-IMAGE=$(gcloud compute images list --filter="family~'ubuntu-2004-lts'" \
+IMAGE=$(gcloud compute images list --filter="family~'ubuntu-2204-lts' AND architecture=X86_64" \
         --sort-by="~creationTimestamp" --format="value(name)" --limit 1 | \
              cut -f1)
 
@@ -33,7 +33,6 @@ then
     echo $NEW_IMAGE_URL
     sed -i "s|\(\"google-machine-image=\).*\"|\1${NEW_IMAGE_URL}\"|" /etc/gitlab-runner/config.toml
     echo "Image successfully updated from '${CUR_IMAGE}' to 'nested-virt-${IMAGE}'"
-
     # Delete all the older images we don't want to keep
     while [ $(echo $CUR_IMAGES | wc -w) -gt $IMAGES_TO_KEEP ]
     do
